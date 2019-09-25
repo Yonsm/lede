aab5a9414153a2854491d0538c7ae5ab9ee59930
Yonsm <Yonsm@qq.com>
2019-09-25 23:41:30 +0800

    Disable autoreboot

--- package/lean/luci-app-autoreboot/luasrc/controller/autoreboot.lua
+++ package/lean/luci-app-autoreboot/luasrc/controller/autoreboot.lua
@@ -1,6 +1,6 @@
 module("luci.controller.autoreboot",package.seeall)
 function index()
-entry({"admin", "system", "autoreboot"}, firstchild(), _("Reboot"), 88).dependent=false
-entry({"admin", "system", "autoreboot", "reboot"}, template("admin_system/reboot"), _("Reboot"), 1)
-entry({"admin", "system", "autoreboot", "auto"}, cbi("autoreboot"), _("Scheduled Reboot"), 2)
+entry({"admin", "system", "reboot"}, firstchild(), _("Reboot"), 88).dependent=false
+entry({"admin", "system", "reboot", "boot"}, template("admin_system/reboot"), _("Reboot"), 1)
+entry({"admin", "system", "reboot", "auto"}, cbi("autoreboot"), _("Scheduled Reboot"), 2)
 end
