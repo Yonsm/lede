f440c5d8cacc3c273cf9ec16bbb3ae9fecfe0b8b
Yonsm <Yonsm@qq.com>
2019-09-25 15:12:56 +0800

    Refine Autoreboot

--- package/lean/luci-app-autoreboot/luasrc/controller/autoreboot.lua
+++ package/lean/luci-app-autoreboot/luasrc/controller/autoreboot.lua
@@ -1,4 +1,6 @@
 module("luci.controller.autoreboot",package.seeall)
 function index()
-entry({"admin","system","autoreboot"},cbi("autoreboot"),_("Scheduled Reboot"),89)
+entry({"admin", "system", "autoreboot"}, firstchild(), _("Reboot"), 88).dependent=false
+entry({"admin", "system", "autoreboot", "reboot"}, template("admin_system/reboot"), _("Reboot"), 1)
+entry({"admin", "system", "autoreboot", "auto"}, cbi("autoreboot"), _("Scheduled Reboot"), 2)
 end
