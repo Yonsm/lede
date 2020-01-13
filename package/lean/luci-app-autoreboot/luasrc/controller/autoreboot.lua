27a2a2f360b661a6568cc0f84c913175314955fc
Yonsm <Yonsm@qq.com>
2020-01-13 16:56:28 +0800

    Refine menu order

--- package/lean/luci-app-autoreboot/luasrc/controller/autoreboot.lua
+++ package/lean/luci-app-autoreboot/luasrc/controller/autoreboot.lua
@@ -1,4 +1,6 @@
 module("luci.controller.autoreboot",package.seeall)
 function index()
-entry({"admin","system","autoreboot"},cbi("autoreboot"),_("Scheduled Reboot"),88)
+entry({"admin", "system", "reboot"}, firstchild(), _("Reboot"), 88).dependent=false
+entry({"admin", "system", "reboot", "boot"}, template("admin_system/reboot"), _("Reboot"), 1)
+entry({"admin", "system", "reboot", "auto"}, cbi("autoreboot"), _("Scheduled Reboot"), 2)
 end
